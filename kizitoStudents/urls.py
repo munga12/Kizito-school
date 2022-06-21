fe2d8de328f1de6ef988e02874e47bbef9925ab2
sankaire <kerenketepela@gmail.com>
2022-06-21 15:33:52 +0300

    feat: register login and signup urls

--- kizitoStudents/urls.py
+++ kizitoStudents/urls.py
@@ -1,6 +1,10 @@
 from . import views
 from django.urls import path
 
+# the foward slash should always come after the url name
+
 urlpatterns = [
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
     path("", views.home, name="home")
 ]
