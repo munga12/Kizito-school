d48111c2bb18610306680e93fa52ba7c1ec618dc
sankaire <kerenketepela@gmail.com>
2022-06-21 15:34:21 +0300

    feat:create login and signup views

--- kizitoStudents/views.py
+++ kizitoStudents/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home/index.html')
+
+def login(request):
+    return render(request, 'auth/login.html')
+def signup(request):
+    return render(request, 'auth/signup.html')
