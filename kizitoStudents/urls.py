49b033923dc96f4feda0ea708b12a80d16fa98f3
sankaire <kerenketepela@gmail.com>
2022-06-25 14:18:58 +0300

    feat: register the dashboard view

--- kizitoStudents/urls.py
+++ kizitoStudents/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 # the foward slash should always come after the url name
 
 urlpatterns = [
+    path("dashboard/", views.dashboard, name="dashboard"),
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
     path("", views.home, name="home")
