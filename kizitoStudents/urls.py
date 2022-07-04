81e5b2c894435b8686462179a33702f4e55170f2
sankaire <kerenketepela@gmail.com>
2022-07-04 14:04:11 +0300

    backend logic, models views

--- kizitoStudents/urls.py
+++ kizitoStudents/urls.py
@@ -4,8 +4,11 @@ from django.urls import path
 # the foward slash should always come after the url name
 
 urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.signIn, name='signIn'),
     path("dashboard/", views.dashboard, name="dashboard"),
-    path("signup/", views.signup, name="signup"),
-    path("login/", views.login, name="login"),
+    path("dashboard/createCourse/", views.createCourse, name="createCourse"),
+    # path("signup/", views.signup, name="signup"),
+    # path("login/", views.login, name="login"),
     path("", views.home, name="home")
 ]
